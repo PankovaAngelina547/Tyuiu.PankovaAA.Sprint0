884483d6f18771dfa9c5d74d32b40dacb613143c
Ангелина Панькова <Tyuiu.PankovaAA@outlook.com>
2025-09-11 18:24:43 +0500

    Task 3

---
+++ Tyuiu.PankovaAA.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.PankovaAA.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.PankovaAA.Sprint0.Task3.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+            Console.ReadKey();
+        }
+    }
+}
